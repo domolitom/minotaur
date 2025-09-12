83442adf8d853af62b760193309575030489a500
Oliver Domokos <domolitom@gmail.com>
2025-09-12 12:44:18 +0200

    add main file which gets data from Binance API

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// BinancePriceResponse represents the relevant field from the API response
+type BinancePriceResponse struct {
+	Symbol string `json:"symbol"`
+	Price  string `json:"price"`
+}
+
+// GetPrice fetches the current price for a given trading pair from Binance
+func GetPrice(symbol string) (BinancePriceResponse, error) {
+	url := fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%s", symbol)
+	resp, err := http.Get(url)
+	if err != nil {
+		return BinancePriceResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var priceResp BinancePriceResponse
+	err = json.NewDecoder(resp.Body).Decode(&priceResp)
+	return priceResp, err
+}
+
+// HTTP handler
+func priceHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.URL.Query().Get("symbol")
+	if symbol == "" {
+		symbol = "BTCUSDT"
+	}
+	priceResp, err := GetPrice(symbol)
+	if err != nil {
+		http.Error(w, "Failed to fetch price from Binance", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(priceResp)
+}
+
+func main() {
+	http.HandleFunc("/price", priceHandler)
+	fmt.Println("Starting server :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
